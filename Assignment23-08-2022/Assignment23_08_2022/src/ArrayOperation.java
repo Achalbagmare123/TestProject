1f2731624bfe063db4b3d4fddfc3083c01c36673
Achalbagmare123 <110631189+Achalbagmare123@users.noreply.github.com>
2022-09-21 13:54:02 +0530

    file 7

---
+++ Assignment23-08-2022/Assignment23_08_2022/src/ArrayOperation.java
@@ -0,0 +1,18 @@
+
+public class ArrayOperation {
+	public static int getSecondSmallest(int[]a){
+		int[] a1 = null;
+		int a2[]= {1,2,3,4,5,6};
+		return a2[1];	
+	}
+
+	public static void main(String[]args) {
+		int[]a2=null;
+		System.out.println("Second smallest = "+getSecondSmallest(a2));
+		
+	}
+
+	
+	}
+
+
---
+++ Assignment23-08-2022/Assignment23_08_2022/src/ArrayReverseNumber.java
@@ -0,0 +1,20 @@
+
+public class ArrayReverseNumber {
+	static void getSorted(int[]a) {
+		int arr;
+	}
+		public static void main(String[] args) {
+			int[]arr = new int[] {1,2,3,4,5};
+			System.out.println(" Array in reverse order");
+			for(int i = arr.length-1;i>=0;i--) {	
+				System.out.println(arr [i]+" ");
+			
+			}
+			System.out.println(" Array in sorted order");
+			for(int i=0;i<arr.length;i++) {
+				System.out.println(arr [i]+" ");	
+			}
+			return;	
+		}
+
+	}
